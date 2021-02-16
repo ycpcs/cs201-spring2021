e4f6ab8fd4a1a5dca0c3c10cfb0c169e7722581a
David Babcock <dbabcock@ycp.edu>
2021-02-16 16:11:37 -0500

    add lectures
    add lectures

--- js/AACONFIG-scheduleInfo.js
+++ js/AACONFIG-scheduleInfo.js
@@ -54,21 +54,21 @@ courseInfo.classPeriods = [
  	{
  		topic: new Topic("** Exam 1", "")
  	},
-// 	{
-// 		topic: new Topic("Lecture 10: Generic methods and classes", "lectures/lecture10.html"),
-// 		reading: "4.5&ndash;4.7",
-// 		lab: new NumberedLabGradle(10, "Using Generic Containers and Algorithms")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 11: Generic methods, functors", "lectures/lecture11.html"),
-// 		reading: "4.8",
-// 		lab: new NumberedLabGradle(11, "Functors")
-// 	},
-// 	{
-// 		topic: new DoubleTopic("Lecture 12: Analysis of Algorithms", "lectures/lecture12.html", "Lecture 13: Big-O", "lectures/lecture13.html"),
-// 		reading: "5.1&ndash;5.3",
-// 		lab: new DoubleNumberedLabGradleNoFile(12, "Benchmarking ArrayList", 13, "Big-O")
-// 	},
+ 	{
+ 		topic: new Topic("Lecture 11: Generic methods and classes", "lectures/lecture11.html"),
+ 		reading: "4.5&ndash;4.7",
+ 		lab: new NumberedLabGradle(11, "Using Generic Containers and Algorithms")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 12: Generic methods, functors", "lectures/lecture12.html"),
+ 		reading: "4.8",
+ 		lab: new NumberedLabGradle(12, "Functors")
+ 	},
+ 	{
+ 		topic: new DoubleTopic("Lecture 13: Analysis of Algorithms", "lectures/lecture13.html", "Lecture 14: Big-O", "lectures/lecture14.html"),
+ 		reading: "5.1&ndash;5.3",
+ 		lab: new DoubleNumberedLabGradleNoFile(13, "Benchmarking ArrayList", 14, "Big-O")
+ 	},
 //	{
 //		topic: new Topic("AWS Setup", ""),
 //		reading: "Canvas videos",
@@ -79,67 +79,67 @@ courseInfo.classPeriods = [
 //		reading: "5.4&ndash;5.8",
 //		lab: new NumberedLabNoFile(13, "Big-O")
 //	},
-// 	{
-// 		topic: new Topic("Lecture 14: Collections, Iterators", "lectures/lecture14.html"),
-// 		reading: "6.1-6.3",
-// 		lab: new NumberedLabGradle(14, "Iterators, Interleaving")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 15: Generic Algorithms", "lectures/lecture15.html"),
-// 		reading: "6.4",
-// 		lab: new NumberedLabGradle(15, "Generic Algorithms")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 16: Lists", "lectures/lecture16.html"),
-// 		reading: "6.5",
-// 		lab: new NumberedLabGradle(16, "List Reversal")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 17: Parallel Programming with Threads", "lectures/lecture17.html"),
-// 		lab: new NumberedLabGradle(17, "Parallel Estimation of π")
-// 	},
-// 	{
-// 		topic: new Topic("Exam 2 Review", "")
-// 	},
-// 	{
-// 		topic: new Topic("** Exam 2", "")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 18: Stacks and Queues", "lectures/lecture18.html"),
-// 		reading: "6.6",
-// 		lab: new NumberedLabGradle(18, "Palindromes")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 19: Sets and Maps", "lectures/lecture19.html"),
-// 		reading: "6.7-6.8",
-// 		lab: new NumberedLabGradle(19, "Word Count")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 20: Recursion", "lectures/lecture20.html"),
-// 		reading: "7.1, 7.3",
-// 		lab: new NumberedLabGradle(20, "Recursion")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 21: Proof by Induction", "lectures/lecture21.html"),
-// 		reading: "7.2",
-// 		lab: new NumberedLabNoFile(21, "Proof by Induction")
-// 	},
-// 	{
-// 		topic: new Topic("Lecture 22: Memoization and Dynamic Programming", "lectures/lecture22.html"),
-// 		reading: "7.6",
-// 		lab: new NumberedLabGradle(22, "Binomial Coefficient")
-// 	},
+ 	{
+ 		topic: new Topic("Lecture 15: Collections, Iterators", "lectures/lecture15.html"),
+ 		reading: "6.1-6.3",
+ 		lab: new NumberedLabGradle(15, "Iterators, Interleaving")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 16: Generic Algorithms", "lectures/lecture16.html"),
+ 		reading: "6.4",
+ 		lab: new NumberedLabGradle(16, "Generic Algorithms")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 17: Lists", "lectures/lecture17.html"),
+ 		reading: "6.5",
+ 		lab: new NumberedLabGradle(17, "List Reversal")
+ 	},
+ 	{
+ 		topic: new Topic("Exam 2 Review", "")
+ 	},
+ 	{
+ 		topic: new Topic("** Exam 2", "")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 18: Parallel Programming with Threads", "lectures/lecture18.html"),
+ 		lab: new NumberedLabGradle(18, "Parallel Estimation of π")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 19: Stacks and Queues", "lectures/lecture19.html"),
+ 		reading: "6.6",
+ 		lab: new NumberedLabGradle(19, "Palindromes")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 20: Sets and Maps", "lectures/lecture20.html"),
+ 		reading: "6.7-6.8",
+ 		lab: new NumberedLabGradle(20, "Word Count")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 21: Recursion", "lectures/lecture21.html"),
+ 		reading: "7.1, 7.3",
+ 		lab: new NumberedLabGradle(21, "Recursion")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 22: Proof by Induction", "lectures/lecture22.html"),
+ 		reading: "7.2",
+ 		lab: new NumberedLabNoFile(22, "Proof by Induction")
+ 	},
+ 	{
+ 		topic: new Topic("Lecture 23: Memoization and Dynamic Programming", "lectures/lecture23.html"),
+ 		reading: "7.6",
+ 		lab: new NumberedLabGradle(23, "Binomial Coefficient")
+ 	},
 // 	{
 // 		topic: new Topic("Lecture 23: Merge and Quick Sort", "lectures/lecture23.html"),
 // 		reading: "8.5-8.6",
 // 		lab: new Lab("No lab", "")
 // 	},
-// 	{
-// 		topic: new Topic("Exam 3 Review", "")
-// 	},
-// 	{
-// 		topic: new Topic("** Exam 3", "")
-// 	},
+ 	{
+ 		topic: new Topic("Exam 3 Review", "")
+ 	},
+ 	{
+ 		topic: new Topic("** Exam 3", "")
+ 	},
 //	{
 //		topic: new DoubleTopic("Lecture 24: Relational Databases and SQL", "lectures/lecture24.html", "Lecture 25: Database Applications and JDBC", "lectures/lecture25.html"),
 //		lab: new DoubleNumberedLab(23, "Relational Databases and SQL", 24, "JDBC")
